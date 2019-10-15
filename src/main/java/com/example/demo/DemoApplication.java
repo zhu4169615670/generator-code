eb28d28e7d534400f2c57748ca071a0167c1e083
pengfei.zhu <pengfei.zhu@baisonmail.com>
2019-10-15 14:13:16 +0800

    优化代码

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author: pengfei.zhu
+ * @description:
+ * @param null
+ * @return:
+ * @author: pengfei.zhu
+ * @time: 2019/10/15 14:13
+ */
 @SpringBootApplication
 public class DemoApplication {
 
